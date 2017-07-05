348890f6cb5920422e7b98e48df30557de5d68bc
DasCodeMonster <pl.lkw.01@gmail.com>
2017-07-05 20:31:06 +0200

    added all

--- chat.py
+++ chat.py
@@ -3,7 +3,7 @@ import threading
 import sys
 # import select
 
-Version = "0.0.2"
+Version = "0.0.3"
 ProjectName = "MyChat"
 connection = False
 thread1 = False
@@ -15,7 +15,7 @@ joined = False
 commands = ["help", "create_room", "leave_room", "join_room", "exit", "join_room_test"]
 
 
-class Thread(threading.Thread):
+class Thread(threading.Thread):  # start a Thread to receive the msg
     def __init__(self, _id, task, host, port):
         threading.Thread.__init__(self)
         self.id = _id
@@ -26,7 +26,7 @@ class Thread(threading.Thread):
     def run(self):
         exec("self." + self.task + "()")
 
-    def create(self):
+    def create(self):  # receive msg in own room
         sock1 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         sock1.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         # sock1.setblocking(0)
@@ -36,19 +36,29 @@ class Thread(threading.Thread):
         connection, addr = sock1.accept()
         # print(type(connection))
         global exitVar
-        while True:
-            if exitVar is True:
-                break
-            else:
-                # print("in running thread", self.id)
-                # ready = select.select([sock1], [], [])
-                # print("meh")
-                # print(ready[0])
-                # if ready[0]:
-                #     msg = connection.recv(1024)
-                #     print(msg.decode("utf-8"))
-                msg = connection.recv(1024)
-                print(msg.decode("utf-8"))
+        try:
+            while True:
+                if exitVar is True:
+                    break
+                else:
+                    # print("in running thread", self.id)
+                    # ready = select.select([sock1], [], [])
+                    # print("meh")
+                    # print(ready[0])
+                    # if ready[0]:
+                    #     msg = connection.recv(1024)
+                    #     print(msg.decode("utf-8"))
+                    msg = connection.recv(1024)
+                    if msg.decode("utf-8").endswith("joined your room!"):
+                        print("system:", msg.decode("utf-8"))
+                    else:
+                        print(msg.decode("utf-8"))
+        except ConnectionAbortedError:
+            global owner
+            owner = False
+            print("Thread ", self.id, " is finished")
+        except ConnectionResetError:
+            print("connectionreseterror")
 
     def joinRoom_alt(self):
         global sock2
@@ -68,28 +78,40 @@ class Thread(threading.Thread):
                 msgIn = sock2.recv(1024)
                 print(msgIn.decode("utf-8"))
 
-
-    def joinRoom(self):
+    def joinRoom(self):  # receive msg in joined room
+        # print("in JoinRoom")
         global sock2
         sock2 = socket.create_connection((self.host, self.port))
+        # print(sock2)
         try:
             try:
                 while True:
+                    # print("in loop")
                     msg = sock2.recv(1024)
                     if msg.decode("utf-8").startswith("/closeSocket"):
                         sock2.close()
+                        print("break")
                         break
                     else:
                         print(msg.decode("utf-8"))
-            except ConnectionResetError(BaseException):
+            except ConnectionResetError:
+                print("connectionreseterror")
                 sock2.close()
                 global joined
                 joined = False
+
+            except ConnectionAbortedError:
+                print("connectionabortederror")
+                sock2.close()
+                joined = False
+
         except TypeError:
+            print("type error")
             sock2.close()
             # global joined
             joined = False
             print("thread finished")
+
 
 def help():
     print("Commands:\n/help\n/create_room\n/leave_room\n/join_room\n/exit")
@@ -98,53 +120,75 @@ def help():
 def exit():
     global exitVar
     exitVar = True
+    global owner
+    global joined
+    global connection
+    global sock2
     if owner is True:
+        connection.send("/closeSocket".encode("utf-8"))
+        connection.close()
+        owner = False
         global thread1
         thread1.join()
-    if joined is True:
+    elif joined is True:
+        sock2.send("/closeSocket".encode("utf-8"))
+        sock2.close()
+        joined = False
         global thread2
         thread2.join()
     sys.exit(0)
 
 
-def create_room():
+def create_room():  # create you own room
     host = ""
     port = 1337
     global thread1
     thread1 = Thread(1, "create", host, port)
     thread1.start()
-    InRoom = True
     global connection
     global exitVar
     global owner
     owner = True
-    while True:
-        if exitVar is False:
-            # msgout = input("Send: ")
-            msgout = input()
-            if msgout.startswith("/"):
-                command = msgout.strip("/")
-                if command in commands:
-                    exec(command + "()")
+    try:
+        while True:
+            if exitVar is False:
+                # msgout = input("Send: ")
+                msgout = input()
+                if msgout.startswith("/"):
+                    command = msgout.strip("/")
+                    if command in commands:
+                        exec(command + "()")
+                    else:
+                        print("Not a command!")
                 else:
-                    print("Not a command!")
+                    if connection is not False:
+                        connection.send(msgout.encode("utf-8"))
             else:
-                if connection is not False:
-                    connection.send(msgout.encode("utf-8"))
-        else:
-            connection.send("/closeSocket")
-            connection.close()
-            break
+                connection.send("/closeSocket".encode("utf-8"))
+                print("exit")
+                connection.close()
+                break
+    except OSError:
+        pass
 
 
-def join_room():
-    host = "127.0.0.1"
+def join_room():  # join an existing room
+    host = socket.gethostbyname(socket.gethostname())
+    # print(host)
     port = 1337
     global thread2
     thread2 = Thread(2, "joinRoom", host, port)
     thread2.start()
     global sock2
+    while True:  # waiting for socket to init
+        if sock2 is not False:
+            break
+        else:
+            # print("waiting for socket")
+            pass
     print("you joined a room")
+    # print(sock2)
+    sock2.send(host.encode("utf-8") + " joined your room!".encode("utf-8"))
     global exitVar
     global joined
     joined = True
@@ -168,6 +212,7 @@ def join_room():
                     except OSError:
                         break
         else:
+            sock2.send("/closeSocket")
             sock2.close()
             print("finished")
             break
@@ -183,11 +228,23 @@ def join_room_test():
         print(msg.decode("utf-8"))
 
 
-def leave_room():
-    pass
+def leave_room():  # leave a room
+    global owner
+    global joined
+    global connection
+    global sock2
+    if owner is True:
+        connection.send("/closeSocket".encode("utf-8"))
+        connection.close()
+        owner = False
+    elif joined is True:
+        sock2.send("/closeSocket".encode("utf-8"))
+        sock2.close()
+        joined = False
+    else:
+        print("you are not in any room")
 
-
-if __name__ == "__main__":
+if __name__ == "__main__":  # start with commandline
     print("Hello welcome to %s!\n Version: %s" % (ProjectName, Version))
     print("For help type /help.")
     while True:
